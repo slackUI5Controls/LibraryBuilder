23432537dc7d361b5ba8eed800696efe74436483
Sean Campbell <brainbug@Seans-MacBook-Pro.local>
2016-03-14 19:45:12 +0000

    Added simple github read functionality

--- wwwroot/Component.js
+++ wwwroot/Component.js
@@ -1,28 +1,50 @@
 sap.ui.define([
-	"sap/ui/core/UIComponent",
-	"sap/ui/Device",
-	"uk/me/seancampbell/model/models"
+    "sap/ui/core/UIComponent",
+    "sap/ui/Device",
+    "uk/me/seancampbell/model/models"
 ], function(UIComponent, Device, models) {
-	"use strict";
+    "use strict";
 
-	return UIComponent.extend("uk.me.seancampbell.Component", {
+    return UIComponent.extend("uk.me.seancampbell.Component", {
 
-		metadata: {
-			manifest: "json"
-		},
+        metadata: {
+            manifest: "json"
+        },
 
 		/**
 		 * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
 		 * @public
 		 * @override
 		 */
-		init: function() {
-			// call the base component's init function
-			UIComponent.prototype.init.apply(this, arguments);
+        init: function() {
+            // call the base component's init function
+            UIComponent.prototype.init.apply(this, arguments);
+            var github = new Github({
+                username: "emoticrab",
+                password: "minimon11",
+                auth: "basic"
+            });
 
-			// set the device model
-			this.setModel(models.createDeviceModel(), "device");
-		}
-	});
+
+            var username = "slackUI5Controls"
+            var reponame = "CustomControls"
+            var repo = github.getRepo(username, reponame);
+            
+            repo.contents("experimental", "/", function(err, contents) {
+                console.log(content)
+            });
+            
+            repo.show(function(err, reps) {
+                console.log(reps)
+             });
+
+
+
+
+
+            // set the device model
+            this.setModel(models.createDeviceModel(), "device");
+        }
+    });
 
 });
--- wwwroot/controller/Main.controller.js
+++ wwwroot/controller/Main.controller.js
@@ -4,7 +4,19 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("uk.me.seancampbell.controller.Main", {
-
+        onPressMasterBack : function() {
+			this.getSplitContObj().backMaster();
+		},
+		onPressGoToMaster : function() {
+			this.getSplitContObj().toMaster(this.createId("subList"));
+		},
+        getSplitContObj : function() {
+			var result = this.byId("SplitCont");
+			if (!result) {
+				jQuery.sap.log.error("SplitApp object can't be found");
+			}
+			return result;
+		}
 	});
 
 });
